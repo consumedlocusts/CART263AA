0ef34e4cec3afb75ccc3143f876117a0f62eea6c
consumedlocusts <samahojabri@gmail.com>
2026-01-14 19:22:58 -0500

    task1:part2

--- p5-rev-template copy/js/script.js
+++ p5-rev-template copy/js/script.js
@@ -8,4 +8,17 @@ function setup() {
 
 function draw() {
   background(0);
+  drawEllipse(20, 30, 60, 50, 400, 500, 500);
+  drawEllipse2(43, 83, 93, 93, 400, 500, 500);
+  drawEllipse3(23, 205, 63, 30, 400, 500, 500);
+}
+
+function drawEllipse(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
+}
+function drawEllipse2(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
+}
+function drawEllipse3(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
 }

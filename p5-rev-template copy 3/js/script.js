3702356174fa1897219e679615520dadd60e4162
consumedlocusts <samahojabri@gmail.com>
2026-01-14 20:36:23 -0500

    m

--- p5-rev-template copy 3/js/script.js
+++ p5-rev-template copy 3/js/script.js
@@ -1,7 +1,9 @@
 "use strict";
-let rect1 = { x: 300, y: 300, w: 40, h: 40 };
-let rect2 = { x: 400, y: 400, w: 40, h: 40 };
-let rect3 = { x: 500, y: 500, w: 40, h: 40 };
+let w = 600 / 3;
+let h = 600;
+let rect1 = { x: 0, y: 0, w: 600 / 3, h: 600, fill: "olive" };
+let rect2 = { x: 600 / 3, y: 0, w: 600 / 3, h: 600, fill: "green" };
+let rect3 = { x: 400, y: 0, fill: "brown" };
 function setup() {
   console.log("go");
   createCanvas(600, 600);
@@ -9,31 +11,33 @@ function setup() {
 
 function draw() {
   background(0);
+  push();
   noStroke();
-  rect(rect1.x, rect1.y, rect1.w, rect1.h);
+  fill(rect1.fill);
+  rect(rect1.x, rect1.y, w, h);
+  pop();
+  push();
   noStroke();
-  rect(rect2.x, rect2.y, rect2.w, rect2.h);
+  fill(rect2.fill);
+  rect(rect2.x, rect2.y, w, h);
+  pop();
+  push();
   noStroke();
-  rect(rect3.x, rect3.y, rect3.w, rect3.h);
+  fill(rect3.fill);
+  rect(rect3.x, rect3.y, w, h);
+  pop();
 }
 
-function mousePressed() {
-  const distance = (mouseX, mouseY, rect1.x, rect1.y);
+function mousePressed2() {
+  const distance = (mouseX, mouseY, rect1.w / 2, rect1.h / 2);
   const mouseOverlapsRect1 = (distance < rect1.x / 2, rect1.y / 2);
-  if (mouseOverlapsRect1 && mousePressed) {
-    rect1.x = 500;
-    rect1.y = 500;
+  if (mouseOverlapsRect1) {
+    rect1.fill = fill(200);
   }
 }
 function keyPressed() {
   if (keyPressed) {
     rect2.x = 20;
     rect2.y = 30;
-  }
-}
-function mouseMoved() {
-  if (mouseMoved) {
-    rect3.x = 95;
-    rect3.y = 200;
   }
 }

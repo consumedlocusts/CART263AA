381bb35edee9b44698fd760326772a0e9b4a7a49
consumedlocusts <samahojabri@gmail.com>
2026-01-14 19:45:51 -0500

    x

--- p5-rev-template copy 2/js/script.js
+++ p5-rev-template copy 2/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
-let rect1 = ((x = 300), (y = 300), (w = 40), (h = 40));
-let rect2 = ((x = 400), (y = 400), (w = 40), (h = 40));
-let rect3 = ((x = 500), (y = 500), (w = 40), (h = 40));
+let rect1 = { x: 300, y: 300, w: 40, h: 40 };
+let rect2 = { x: 400, y: 400, w: 40, h: 40 };
+
 function setup() {
   console.log("go");
   createCanvas(600, 600);
@@ -9,12 +9,14 @@ function setup() {
 
 function draw() {
   background(0);
+  noStroke();
+  rect(rect1.x, rect1.y, rect1.w, rect1.h);
 }
 
 function mousePressedRect() {
   const distance = (mouseX, mouseY, rect1.x, rect1.y);
-  const mouseOverlapsRect1 = distance < rect1.x / 2;
-  if (mousePressed && mouseOverlapsRect1) {
-    rect1.x = 400;
+  const mouseOverlapsRect1 = (distance < rect1.x / 2, rect1.y / 2);
+  if (mouseOverlapsRect1) {
+    rect1.x = rect1.x / 2;
   }
 }
--- p5-rev-template copy/js/script.js
+++ p5-rev-template copy/js/script.js
@@ -1,7 +1,6 @@
 "use strict";
-let rect1 = ((x = 300), (y = 300), (w = 40), (h = 40));
-let rect2 = ((x = 400), (y = 400), (w = 40), (h = 40));
-let rect3 = ((x = 500), (y = 500), (w = 40), (h = 40));
+let x, y, w, h, r, g, b;
+
 function setup() {
   console.log("go");
   createCanvas(600, 600);
@@ -9,12 +8,17 @@ function setup() {
 
 function draw() {
   background(0);
+  drawEllipse(20, 30, 60, 50, 400, 500, 500);
+  drawEllipse2(43, 83, 93, 93, 400, 500, 500);
+  drawEllipse3(23, 25, 63, 3, 400, 500, 500);
 }
 
-function mousePressedRect() {
-  const distance = (mouseX, mouseY, rect1.x, rect1.y);
-  const mouseOverlapsRect1 = distance < rect1.x / 2;
-  if (mousePressed && mouseOverlapsRect1) {
-    rect1.x = 400;
-  }
+function drawEllipse(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
+}
+function drawEllipse2(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
+}
+function drawEllipse2(x, y, w, h, r, g, b) {
+  ellipse(x, y, h, w, r, g, b);
 }

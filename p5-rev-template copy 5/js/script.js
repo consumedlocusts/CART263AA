981bb00799ba9e49f02a3d69f112666a617afac6
consumedlocusts <samahojabri@gmail.com>
2026-01-15 22:17:40 -0500

    q

--- p5-rev-template copy 5/js/script.js
+++ p5-rev-template copy 5/js/script.js
@@ -1,10 +1,28 @@
 "use strict";
 //p5ok
 let word = "test";
-
+let string;
+let xw = 24; //step per pixel between numebrs
+let yh = 20; //margin of the pixels
 function setup() {
-  createCanvas(800, 800);
-
+  createCanvas(400, 400);
   textAlign(CENTER, CENTER);
+}
+function draw() {
+  background(20);
+  fill("white");
+
   textSize(28);
+  text(word, width / 2, height / 2);
+
+  for (let i = 0; i <= 9; i++) {
+    let x = yh + i * xw;
+    let y = 20;
+    text(i, x, y);
+  }
+  for (let i = 0; i <= 15; i++) {
+    let x = 20;
+    let y = yh + i * xw;
+    text(i, x, y);
+  }
 }

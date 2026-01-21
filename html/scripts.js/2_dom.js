debc06482d030eca3817fcd248f6da23d4eb1d5b
consumedlocusts <samahojabri@gmail.com>
2026-01-21 18:35:14 -0500

    addingsHtml

---
+++ html/scripts.js/2_dom.js
@@ -0,0 +1,7 @@
+window.onload = setup;
+function setup() {
+  console.log("running setup");
+  console.log(document.getElementById("one"));
+  console.log(document.querySelector("#one"));
+  console.log(document.getElementsByTagName("div"));
+}

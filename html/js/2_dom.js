dc5d9cba1b0a705133da2334edcb1cdc6ccb517b
consumedlocusts <samahojabri@gmail.com>
2026-01-22 16:53:44 -0500

    yes

--- html/js/2_dom.js
+++ html/js/2_dom.js
@@ -2,9 +2,27 @@ window.onload = setup;
 //hii
 function setup() {
   console.log("running setup");
-  let second_element = document.getElementById("one");
-  let second_again = document.querySelector("#one");
-  console.log(document.getElementsByTagName("div"));
-  console.log(second_element);
-  console.log(second_again);
+  // let second_element = document.getElementById("one");
+  // let second_again = document.querySelector("#one");
+  // console.log(document.getElementsByTagName("div"));
+  // console.log(second_element);
+  // console.log(second_again);
+
+  // let elements= document.getElementsByTagName("div");
+  //  console.log(elements[0]);
+  //  console.log(elements[1]);
+  //  console.log(elements[2]);
+
+  //  for (let i=0; i<elements.length i++){
+  //   console.log(elements[i])
+  //  }
+  //  let firstDiv= document.querySelector("div")
+  //  let allDivs=document.querySelectorAll("div")
+  //  console.log(allDivs)
+  //  let elementsWClass=document.getElementsByClassName
+
+  //  let firstEl=document.
+  // }
+  let idFiv = document.querySelector(".square_shape").getAttribute("id");
+  console.log(idFiv);
 }

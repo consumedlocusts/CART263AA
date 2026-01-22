feffdf17bd2508175f2b5587f7240133980a4da3
consumedlocusts <samahojabri@gmail.com>
2026-01-21 19:44:59 -0500

    rename

---
+++ html/js/2_dom.js
@@ -0,0 +1,10 @@
+window.onload = setup;
+//hii
+function setup() {
+  console.log("running setup");
+  let second_element = document.getElementById("one");
+  let second_again = document.querySelector("#one");
+  console.log(document.getElementsByTagName("div"));
+  console.log(second_element);
+  console.log(second_again);
+}

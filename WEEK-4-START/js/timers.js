8512fe66052cedc9d76bd7fa06623262480be611
consumedlocusts <samahojabri@gmail.com>
2026-02-04 19:13:37 -0500

    week4

--- WEEK-4-START/js/timers.js
+++ WEEK-4-START/js/timers.js
@@ -11,21 +11,21 @@ window.onload = function () {
   ];
 
   console.log("timers running");
-  for (let i = 0; i < 24; i++) {
-    //for each x - make a column of changing y's
-    for (let j = 0; j < 24; j++) {
-      //create a grid cell with a div
-      let parent = document.getElementById("parent");
-      let d = document.createElement("div");
-      d.classList.add("grid-cell");
-      parent.appendChild(d);
+  //   for (let i = 0; i < 24; i++) {
+  //     //for each x - make a column of changing y's
+  //     for (let j = 0; j < 24; j++) {
+  //       //create a grid cell with a div
+  //       let parent = document.getElementById("parent");
+  //       let d = document.createElement("div");
+  //       d.classList.add("grid-cell");
+  //       parent.appendChild(d);
 
-      d.style.left = (i + 1) * 25 + "px";
-      d.style.top = (j + 1) * 25 + "px";
-    }
-  }
-  let gridCells = document.querySelectorAll(".grid-cell");
-  let divisor = 2;
+  //       d.style.left = (i + 1) * 25 + "px";
+  //       d.style.top = (j + 1) * 25 + "px";
+  //     }
+  //   }
+  //   let gridCells = document.querySelectorAll(".grid-cell");
+  //let divisor = 2;
   //   for (let i = 0; i < gridCells.length; i++) {
   //     // if(i%divisor===0){
   //     //     gridCells
@@ -40,10 +40,58 @@ window.onload = function () {
   //     gridCells[i].style.background = shades[currentShadeIndex];
   //   }
 
-  let changingDivisor = 0;
-  window.setInterval(animate_rows, 1000);
-  function animate_rows() {
-    changingDivisor += 1;
-    console.log(changingDivisor);
+  //   let changingDivisor = 0;
+  //   //window.setInterval(animate_rows, 1000);
+  //   let ref = window.setInterval(animate_rows, 500);
+  //   function animate_rows() {
+  //     changingDivisor += 1;
+  //     console.log(changingDivisor);
+  //     drawGrid();
+  //     if (changingDivisor === 12) {
+  //       clearInterval(ref);
+  //     }
+  //   }
+  //   function drawGrid() {
+  //     for (let index = 0; index < gridCells.length; index++) {
+  //       //check what the remainder is ...
+  //       if (index % changingDivisor === 0) {
+  //         gridCells[index].style.background = shades[0];
+  //       } else if (index % changingDivisor === 1) {
+  //         gridCells[index].style.background = shades[1];
+  //       } else if (index % changingDivisor === 2) {
+  //         gridCells[index].style.background = shades[2];
+  //       } else if (index % changingDivisor === 3) {
+  //         gridCells[index].style.background = shades[3];
+  //       } else if (index % changingDivisor === 4) {
+  //         gridCells[index].style.background = shades[4];
+  //       } else if (index % changingDivisor === 5) {
+  //         gridCells[index].style.background = shades[5];
+  //       } else if (index % changingDivisor === 6) {
+  //         gridCells[index].style.background = shades[6];
+  //       } else if (index % changingDivisor === 7) {
+  //         gridCells[index].style.background = shades[7];
+  //       }
+  //     }
+  //   }
+  //   let dynamicdelay = 500;
+  //   window.setInterval(function (e) {
+  //     let sp = document.createElement("span");
+  //     sp.textContent = "adding text";
+  //     document.querySelector("#parent").appendChild(sp);
+  //     dynamicdelay -= 100;
+  //     console.log(dynamicdelay);
+  //   }, dynamicdelay);
+  // };
+  let dynamicdelay = 500;
+  window.setTimeout(changingTimeout, dynamicdelay);
+
+  function changingTimeout() {
+    let sp = document.createElement("span");
+    sp.textContent = "adding text";
+    document.querySelector("#parent").appendChild(sp);
+    dynamicdelay -= 10;
+    console.log(dynamicdelay);
+    //dont do set interval again use set timeout for dynamic delays
+    window.setTimeout(changingTimeout, dynamicdelay);
   }
 };

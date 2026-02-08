ae9daace0b3ac0f67e1090e06a5ea062208c5424
consumedlocusts <samahojabri@gmail.com>
2026-02-08 15:30:30 -0500

    notes E team

--- CART263-TASK-3/task-3-js/team-e.js
+++ CART263-TASK-3/task-3-js/team-e.js
@@ -8,8 +8,7 @@ function setup_E() {
   console.log("in e");
   /**************************************************** */
   //get the buttons
-  activateButtons(`#TEAM_E`, "ani_canvE",aniA,aniB,aniC,aniD);
-
+  activateButtons(`#TEAM_E`, "ani_canvE", aniA, aniB, aniC, aniD);
 
   /**************** ANI A ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN A INSIDE HERE */
@@ -26,18 +25,17 @@ function setup_E() {
    * remember you can define other functions inside....
    * Do not change any code above or the HTML markup.
    * **/
-   
+
   function aniA(parentCanvas) {
     console.log("in ani-A -teamE");
   }
-
 
   /****************ANI B ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN B INSIDE HERE */
   /****************ANI B ************************************ */
   /**************** TASK *******************************************
    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
-   * 1: create a creatve, visual pattern using text, divs as shapes, images ... 
+   * 1: create a creatve, visual pattern using text, divs as shapes, images ...
    * 2: add in mouseover event listener(s) somewhere to make the sketch interactive
    *
    * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
@@ -49,8 +47,7 @@ function setup_E() {
    * **/
 
   function aniB(parentCanvas) {
-      console.log("in ani-B -teamE");
-    
+    console.log("in ani-B -teamE");
   }
   /****************ANI C ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE HERE */
@@ -59,7 +56,7 @@ function setup_E() {
    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:)
    * 1: use the PROVIDED keyup/down callbacks `windowKeyDownRef` and/or `windowKeyUpnRef` to handle keyboard events
    * 2: create an interactive pattern/sketch based on keyboard input. Anything goes.
-   * 
+   *
    * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
    * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
    * this is so that your styles are not overriden by other teams.
@@ -71,21 +68,58 @@ function setup_E() {
   /* TASK: make an interactive pattern .. colors, shapes, sizes, text, images....
    * using  ONLY key down and/or keyup -- any keys::
    */
-  window.onload=function(){
-console.log("TEAM E, Sama's image grid thhing")
-//team e's canvas div from DOM id
-let parentCanvas=document.getElementById("ani_canvE_A");
-//gridding
-makeGrid(parentCanvas);
-//event listener for keyboardpress, listen 
-window.addEventListener("keydown",function(event)){
-  let stepMove=10; //this is how many pixels per each keypress shifts 
 
-}
+  console.log("TEAM E, Sama's image grid thhing HOVERINGG");
+  //team e's canvas div from DOM id
+  let imgPath = "teamEimages/oneImage.png"; //soon i will upload the image, but for now this is the new team E file path
+  let step = 20; //grid layout: spacing between cells
+  let tileSize = 18; //grid layout: actual tile sizes
+  let totalTiles = 0; //for track revealing bool progress
+  let revealedT = 0;
+  parentCanvas.innerHTML = "";
+  let box = parentCanvas.getBoundingClientRect(); //rendered size of thhis canvas to get
+  //building the initial "hidden" style grid with nests
+  for (let y = step; y < box.height; y += step) {
+    for (letx = step; x < box.width; x += step) {
+      let tile = document.createElement("div");
+      tile.classList.add(".TEAM_E_cell");
+      //placing tiles
+      tile.style.left = x + "px";
+      tile.style.top = y + "px";
+      tile.style.width = tileSize + "px";
+      tile.style.height = tileSize + "px";
+      //keeping the same image fo every tile but its hidden by the inital first layer of red grids
+      //use image file as bacjground image for/as specified div tile
+      tile.style.backgroundImage = `url(${imgPath})`;
+      //stroing tile pos for image slicing
+      tile.setAttribute("x", x);
+      tile.setAttribute("y", y);
+      //track if tile haas been revealed
+      tile.setAttribute("revealed", "no");
+      //hover reveal effect listener
+      tile.addEventListener("mousemove", revealTile);
+      //counter
+      totalTiles += 1;
+    }
   }
-
+  function revealTile() {
+    //"this" is the tile div the mouse is currently over
+    //parseInt converts attrb strings into numbers
+    let x = parseInt(this.getAttribute("x"));
+    let y = parseInt(this.getAttribute("y"));
+    //show correct slice of the only (one) image inside this tile
+    //the bgative background pos is saying"move image left/up under the winow"
+    this.style.backgroundPosition = -x + "px " + -y + "px";
+    //make tile visible bt removing the red layer mask
+    this.style.backgroundColor = "transparent";
+    //and coubt this tile as revealed only once
+    if (this.getAttribute("revealed") === "no") {
+      this.setAttribute("revealed", "yes");
+      revealedT += 1;
+    }
+  }
   function aniC(parentCanvas) {
-      console.log("in ani-C -teamE");
+    console.log("in ani-C -teamE");
 
     /*** THIS IS THE CALLBACK FOR KEY DOWN (* DO NOT CHANGE THE NAME *..) */
     windowKeyDownRef = function (e) {
@@ -119,9 +153,7 @@ window.addEventListener("keydown",function(event)){
    * remember you can define other functions inside....
    * Do not change any code above or the HTML markup.
    * **/
-    function aniD(parentCanvas) {
-      console.log("in ani-D -teamE");
-    }
-
+  function aniD(parentCanvas) {
+    console.log("in ani-D -teamE");
+  }
 }
-   

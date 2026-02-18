96efc4e8ce33860efdeff9ad5f465db626b8707b
consumedlocusts <samahojabri@gmail.com>
2026-02-18 18:57:15 -0500

    k

--- CART263-OBJECTS-III/js/bird.js
+++ CART263-OBJECTS-III/js/bird.js
@@ -9,6 +9,31 @@ class Bird extends Animal {
     this.vx = Math.random() * 5 + 1;
     this.vy = 0;
     this.animalBody = document.createElement("div");
+    this.originalY = this.y;
+
+    //ONLY in the Bird class : new variables
+    this.angle = 0;
+    this.sleepiness = 0.1;
+  }
+
+  // override A - p1
+  // Move the Animal according to its velocity
+  move() {
+    //console.log("go");
+
+    this.y = this.originalY + Math.sin(this.angle) * 8;
+    this.angle += 0.05;
+    this.veer();
+    super.move();
+  }
+  // p2
+  // veer() causes the bird to randomly veer on the y axis
+  veer() {
+    let r = Math.random();
+    //console.log("in veer "+r)
+    if (r < this.sleepiness) {
+      this.vy += randomRange(-0.1, 0.1);
+    }
   }
 
   // Display the bird as a ellipse
@@ -22,7 +47,9 @@ class Bird extends Animal {
     this.animalBody.style.top = this.y + "px";
     this.animalBody.style.borderRadius = this.width + "px";
     this.animalBody.style.backgroundColor = `rgb(106, 90, 205)`;
-    //add to the DOM
     document.getElementsByClassName("sky")[0].appendChild(this.animalBody);
   }
+}
+function randomRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }

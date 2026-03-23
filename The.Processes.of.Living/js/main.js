21eb43e15f9c4a4d7f42e585339dc1d3af29842c
consumedlocusts <samahojabri@gmail.com>
2026-03-23 16:50:33 -0400

    clean main

--- The.Processes.of.Living/js/main.js
+++ The.Processes.of.Living/js/main.js
@@ -11,43 +11,9 @@ function setupManager() {
 
 //return button when ready
 
-function setupButtons() {
-  document.getElementById("future-return");
-  document.addEventListener("click", function () {
-    //  manager.switchTo("opening");
-  });
-}
-
-// class StateManager {
-//   constructor() {
-//     //all elements of states storage
-//     this.states = {};
-//     //current state visible storeage
-//     this.current = null;
-//   }
-//   add(name, state) {
-//     //store the element o state by name
-//     this.states[name] = state;
-//   }
-//   switch(name) {
-//     //hide current is theres alreayd another activated
-//     if (this.current) {
-//       this.current.style.display = "none";
-//     }
-//     //set new current state
-//     this.current = this.states[name];
-//     //show it
-//     this.current.style.display = "block";
-
-//     // if (name === "future") {
-//     //   futureThe.enter;
-//     // }
-//   }
-// }
-// //sorrry i wrote this in the wrong js
-// class TheFuture {
-//   //get element ID stuff
-//   constructor() {
-//     this.canvas = document.getElementById("future-canvas");
-//   }
+// function setupButtons() {
+//   document.getElementById("future-return");
+//   document.addEventListener("click", function () {
+//     //  manager.switchTo("opening");
+//   });
 // }

703ac7f0d0690a36061fbd715519c902fdc9dae9
consumedlocusts <samahojabri@gmail.com>
2026-04-08 19:46:12 -0400

    n

--- tonejsTESTproject/toner.js
+++ tonejsTESTproject/toner.js
@@ -1,15 +1,17 @@
-//npm install tone      // Install the latest stable version
 import * as Tone from "tone";
-const synth = new Tone.Synth().toDestination();
+import * as THREE from "three";
+//npm install tone      // Install the latest stable version
+console.log(Tone);
+const synth = Tone.Synth().toDestination();
 const now = Tone.now();
 // trigger the attack immediately
 synth.triggerAttack("C4", now);
 // wait one second before triggering the release
 synth.triggerRelease(now + 1);
-function playNote() {
-  // create a synth
-  const synth = new Tone.Synth().toDestination();
-  // play a note from that synth
-  synth.triggerAttackRelease("C4", "8n");
-}
+// function playNote() {
+//   // create a synth
+//   const synth = new Tone.Synth().toDestination();
+//   // play a note from that synth
+//   synth.triggerAttackRelease("C4", "8n");
+// }
 //https://nodejs.org/en/download

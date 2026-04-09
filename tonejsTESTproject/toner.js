695870401aa001c97a0ce03d4b752810142b1f3d
consumedlocusts <samahojabri@gmail.com>
2026-04-08 20:24:20 -0400

    k

--- tonejsTESTproject/toner.js
+++ tonejsTESTproject/toner.js
@@ -1,13 +1,83 @@
-import * as Tone from "tone";
 import * as THREE from "three";
-//npm install tone      // Install the latest stable version
-console.log(Tone);
-const synth = Tone.Synth().toDestination();
-const now = Tone.now();
-// trigger the attack immediately
-synth.triggerAttack("C4", now);
-// wait one second before triggering the release
-synth.triggerRelease(now + 1);
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+//A: the geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+//B: the material
+const material = new THREE.MeshBasicMaterial({ color: 0x800080 });
+//C: put together
+const mesh = new THREE.Mesh(geometry, material);
+//D: ADD TO THE SCENE
+scene.add(mesh);
+const sizes = {
+  width: 800,
+  height: 600,
+};
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+scene.add(camera);
+//Access the Canvas
+const canvas = document.querySelector("canvas#three-ex");
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+//give it the size
+
+//move camera
+camera.position.z = 3;
+const controls = new OrbitControls(camera, renderer.domElement);
+//TURN ON AXES HELPER
+//https://threejs.org/docs/?q=Axes#AxesHelper
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+//move it
+axesHelper.position.x = -1;
+axesHelper.position.y = -1;
+const mesh_2 = new THREE.Mesh(geometry, material);
+scene.add(mesh_2);
+mesh_2.position.x = 1.5;
+mesh_2.position.y = 1.25;
+mesh_2.position.z = -1;
+renderer.setSize(sizes.width, sizes.height);
+
+//render:
+renderer.render(scene, camera);
+//A: the geometry
+
+mesh.scale.x = 2;
+mesh.scale.y = 0.25;
+mesh.scale.z = 0.5;
+//const material = new THREE.MeshBasicMaterial()
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.5, 0.3, 16, 32),
+  material,
+);
+torus.position.x = 1.5;
+
+scene.add(sphere, plane, torus);
+// const water_texture = await loader.loadAsync(
+//   "textures/Ice002_1K-JPG_Color.jpg",
+// );
+// //need to ensure that the textures are encoded correctly - mapping the colors correctly.
+// water_texture.colorSpace = THREE.SRGBColorSpace;
+
+// const synth = new Tone.Synth().toDestination();
+// const now = Tone.now();
+// // trigger the attack immediately
+// synth.triggerAttack("C4", now);
+// // wait one second before triggering the release
+// synth.triggerRelease(now + 1);
 // function playNote() {
 //   // create a synth
 //   const synth = new Tone.Synth().toDestination();
@@ -15,3 +85,11 @@ synth.triggerRelease(now + 1);
 //   synth.triggerAttackRelease("C4", "8n");
 // }
 //https://nodejs.org/en/download
+
+document.querySelector("#play").addEventListener("click", playNote);
+function playNote() {
+  // create a synth
+  const synth = new Tone.Synth().toDestination();
+  // play a note from that synth
+  synth.triggerAttackRelease("C4", "8n");
+}
